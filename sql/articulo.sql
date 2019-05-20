b3f98264eba5c65ead9822a88fc45f854e54fbe5
Carlos Steven <esteven96@gmail.com>
2019-05-19 22:10:13 -0500

    v 1.0.2 fix articulos

--- sql/articulo.sql
+++ sql/articulo.sql
@@ -5,7 +5,7 @@
 CREATE TABLE public.articulo
 (
     art_id integer NOT NULL DEFAULT nextval('articulo_art_id_seq'::regclass) ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    art_issn text COLLATE pg_catalog."default",
+    art_isbn text COLLATE pg_catalog."default",
     art_doi text COLLATE pg_catalog."default",
     art_ind text COLLATE pg_catalog."default",
     art_idi text COLLATE pg_catalog."default",
@@ -16,6 +16,7 @@ CREATE TABLE public.articulo
     art_pag_ini bigint,
     art_pag_fin bigint,
     art_urt text COLLATE pg_catalog."default",
+    art_res text COLLATE pg_catalog."default",
     CONSTRAINT articulo_pkey PRIMARY KEY (art_id)
 )
 WITH (
